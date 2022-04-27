a780c923a17a023cb12e0eae5341c45028ada886
Ioana Seres <ioana.cosoreanuseres@888holdings.com>
2022-04-27 09:54:55 +0300

    sort filter ch

--- main.js
+++ main.js
@@ -48,18 +48,21 @@ const customers = {
 		industry: "Banking & Finance",
 		country: "Germany",
 		relevance: 7,
+		image:"./images/clients/allianz.svg"
 	 },
 	  {
 		name: "Deloitte",
 		industry: "Audit & Advisory",
 		country: "United Kingdom",
 		relevance: 7,
+		image:'./images/clients/deloitte.svg'
 	  },
 	  {
 		name: "Cathay United Bank",
 		industry: "Banking & Finance",
 		country: "China",
 		relevance: 1,
+		image:"./images/clients/cathay.svg"
 	
 	  },
 	  {
@@ -67,81 +70,186 @@ const customers = {
 		industry: "Technology & Telecom",
 		country: "United States of America",
 		relevance: 10,
-	
+		image:'./images/clients/intel.svg'
 	  },
 	  {
 		name: "Yale University",
 		industry: "Education",
 		country: "United States of America",
 		relevance: 10,
-	
+		image:"./images/clients/yale.svg"
 	  },
 	  {
 		name: "Orange",
 		industry: "Technology & Telecom",
 		country: "France",
 		relevance: 9,
-	
+		image:"./images/clients/orange.svg"
 	  },
 	  {
 		name: "Johnson & Johnson",
 		industry: "Healthcare",
 		country: "United Kingdom",
 		relevance: 8,
-	
+		image:'./images/clients/johnson.svg'
 	  },
 	  {
 		name: "Vodafone",
 		industry: "Technology & Telecom",
 		country: "United Kingdom",
 		relevance: 9,
-	
+		image:'./images/clients/vodafone.svg'
 	  },
 	  {
 		name: "Accenture",
 		industry: "Audit & Advisory",
 		country: "United Kingdom",
 		relevance: 7,
-	
+		image:"./images/clients/accenture.svg"
 	  },
 	  {
 		name: "U.S. Department of Commerce",
 		industry: "Gov & Public Sector",
 		country: "United States of America",
 		relevance: 10,
-		
+		image:'./images/clients/gov.svg'
 	  },
 	]
   }
 
+  //displaying cards
+
+  const cardContainer = document.querySelector(".CustomersItems");
+
+  const displayCards = function(card, sort = false){
+	cardContainer.innerHTML="";
+	card.forEach(element=>{
+		
+	   const html= `
+	   <li class="CustomersItem">
+	   <img src=${element.image} alt="${element.name} is a Pentest-Tools customer" class="CustomersImage" title="${element.name} is a Pentest-Tools customer" width="215" height="60" data-name="${element.name}" data-relevance="${element.relevance}">
+   		</li>
+	   `
+
+	   cardContainer.insertAdjacentHTML("afterbegin", html)
+
+  
+	})}
+	displayCards(customers.data, false);
+
   //sort
-  const sortSelect = document.querySelector(".Select");
+
   const ascending = document.getElementById("ascending");
   const descending = document.getElementById("descending");
 
 
-  const customersSortedAsc = [...customers.data].sort((a,b) => a.relevance-b.relevance);
-  const customersSortedDesc = [...customers.data].sort((a,b) => b.relevance-a.relevance);
-  console.log(customersSortedAsc);
-  console.log(customersSortedDesc);
+const customersSortedAscRelevance = [...customers.data].sort((a,b) => a.relevance-b.relevance);
+const customersSortedDescRelevance = [...customers.data].sort((a,b) => b.relevance-a.relevance);
 
-  ascending.addEventListener("click", function(e){
-	  e.preventDefault();
+const customersSortedAscAlphabetical = [...customers.data].sort((a,b) => a.name > b.name ? -1 : 1);
+const customersSortedDescAlphabetical = [...customers.data].sort((a,b) => a.name < b.name ? -1 : 1);
+
+ascending.addEventListener("click", function(e){
+	e.preventDefault();
+	displayCards(customersSortedAscRelevance);
+	ascending.style.display = "none";
+	descending.style.display = "block";
+	 
   })
   descending.addEventListener("click", function(e){
 	e.preventDefault();
-	
+	displayCards(customersSortedDescRelevance);
+	descending.style.display = "none";
+	ascending.style.display = "block";
+})
+const sortSelect = document.querySelector(".Select");
+
+sortSelect.addEventListener("change", function (event) {
+	event.preventDefault();
+
+// if(event.target.value === "default") displayCards(customers.data, false);
+
+  if(event.target.value === "relevance"){
+
+		ascending.addEventListener("click", function(e){
+		e.preventDefault();
+		displayCards(customersSortedAscRelevance);
+		ascending.style.display = "none";
+		descending.style.display = "block";
+		 
+	  })
+	  descending.addEventListener("click", function(e){
+		e.preventDefault();
+		displayCards(customersSortedDescRelevance);
+		descending.style.display = "none";
+		ascending.style.display = "block";
+	})
+ } 
+
+ else if(event.target.value === "alphabetical"){
+	ascending.addEventListener("click", function(e){
+		e.preventDefault();
+		displayCards(customersSortedAscAlphabetical);
+		ascending.style.display = "none";
+		descending.style.display = "block";
+		 
+	  })
+	  descending.addEventListener("click", function(e){
+		e.preventDefault();
+		displayCards(customersSortedDescAlphabetical);
+		descending.style.display = "none";
+		ascending.style.display = "block";
+	})
+ }
 })
 
-sortSelect.addEventListener("change", function(e){
-	console.log(e.currentTarget);
-})
 
 // filter
 
-const filteredCustomers = [...customers.data].filter(value => console.log(value.industry) );
+const filterByCountry = (country) => {
+
+	country = country.toLowerCase();
+	if (country === "All countries") return customers.data;
+  
+	const filterResult = customers.data.filter(el => {
+	  return el.country.toLowerCase().includes(country)
+	  })
+	return filterResult;
+  
+  }
+
+  const selectCountry = document.querySelector("#actionFilterFirst");
+
+	selectCountry.addEventListener("change", function (event) {
+ 
+ 	cardContainer.innerHTML = "";
+
+  const results = filterByCountry(event.target.value);
+ 	displayCards(results);
+})
 
 
+const filterByIndustry = (industry) => {
+
+	industry = industry.toLowerCase();
+	if (industry === "All industries") return customers.data;
+  
+	const filterResult = customers.data.filter(el => {
+	  return el.industry.toLowerCase().includes(industry)
+	  })
+	return filterResult;
+  
+  }
+
+  const selectIndustry = document.querySelector("#actionFilterSecond");
+
+	selectIndustry.addEventListener("change", function (event) {
+ 
+ 	cardContainer.innerHTML = "";
+
+  const results = filterByIndustry(event.target.value);
+	 displayCards(results);
+})
 // starting Service worker 
 // if ('serviceWorker' in navigator) {
 // 	navigator.serviceWorker.register('service-worker.js');
